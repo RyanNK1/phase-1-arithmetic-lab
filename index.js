0cd1467c85abf9d3119864d2d4465d40c8ec6a3e
ryan <ryan.kimani0@gmail.com>
2023-03-21 13:21:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-
 // Write your code here
+let num1 = 31
+let num2 = 2
+let multiply
+multiply = num1*num2
+let random 
+random = Math.floor(Math.random()*10)
+
+let num3 = 12
+let num4 = 8
+let mod
+mod = num3%num4
+
+let max 
+max = Math.max(2,5,15,20,12)
